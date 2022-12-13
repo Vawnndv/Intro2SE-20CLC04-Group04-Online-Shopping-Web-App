8eed9f386bc0d00723f49b9f5941bc3758a7de81
Vawnndv <dinhvanguyen810@gmail>
2022-12-13 15:19:29 +0700

    BE tạo Axios - update homepage.js

--- frontend/src/components/homepage/homepage.js
+++ frontend/src/components/homepage/homepage.js
@@ -2,31 +2,41 @@ import './homepage.css'
 import '../stylesheet/style.css'
 import data from '../../data.js'
 import {Link} from "react-router-dom";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
-var Homepage = () => {
+let Homepage = () => {
+    const [products, setProduct] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios.get('/api/products');
+            setProduct(result.data);
+        };
+        fetchData();
+    }, []);
     return (
-        <main>
+        <div className="Homepage-main">
             <h1>Các sản phẩm nổi bật</h1>
             <div className="products">
-            {
-                data.products.map((product) => (
-                    <div className="product" key={product.slug}>
-                        <Link to={`/product/${product.slug}`}>
-                            <img src={product.image} alt={product.name} />
-                        </Link>
-                        
-                        <Link to={`/product/${product.slug}`}>
-                            <div className="product-info">
-                                <p>{product.name}</p>
-                                <p>{product.price}đ</p>
-                            </div>
-                        </Link>
-                        
-                    </div>
-                ))
-            }
+                {
+                    products.map((product) => (
+                        <div className="product" key={product.slug}>
+                            <Link to={`/product/${product.slug}`}>
+                                <img src={product.image} alt={product.name}/>
+                            </Link>
+
+                            <Link to={`/product/${product.slug}`}>
+                                <div className="product-info">
+                                    <p>{product.name}</p>
+                                    <p>{product.price}đ</p>
+                                </div>
+                            </Link>
+
+                        </div>
+                    ))
+                }
             </div>
-        </main>
+        </div>
     );
 };
 export default Homepage;
