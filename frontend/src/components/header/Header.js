e8b8e856f689aeaf4f431ae953b012cdbb731aed
disisVu <ngoanhvu007@gmail.com>
2022-12-13 13:57:07 +0700

    update Header & import FontAwesome

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,44 +1,49 @@
 import "./Header.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 var Header = () => {
     return (
-        <header className="container-fluid">
-            <div className="container">
-            <nav className="navbar navbar-expand-md">
-                <div className="navbar-nav me-auto">
-                <a href="/" className="nav-item nav-link ms-5">About us</a>
+        <header>
+            <div className="navbar-container container-fluid">
+                <div className="container">
+                    <nav className="navbar navbar-expand p-0">
+                        <div className="navbar-nav ms-auto d-flex align-items-center">
+                            <a href="/" className="nav-item nav-link ms-5">About us</a>
+                            <div className="nav-item dropdown ms-5">
+                                <a href="/" className="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">User</a>
+                                <div className="dropdown-menu dropdown-menu-end">
+                                    <a href="/" className="dropdown-item">My account</a>
+                                    <a href="/" className="dropdown-item">My orders</a>
+                                    <a href="/" className="dropdown-item">Sign out</a>
+                                </div>
+                            </div>
+                        </div>
+                    </nav>
                 </div>
-                <div className="navbar-nav ms-auto">
-                <div className="nav-item dropdown ms-5">
-                    <a href="/" className="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">User</a>
-                    <div className="dropdown-menu dropdown-menu-end">
-                    <a href="/" className="dropdown-item">My account</a>
-                    <a href="/" className="dropdown-item">My orders</a>
-                    <a href="/" className="dropdown-item">Sign out</a>
+            </div>
+            <div className="hero-container container-fluid d-flex justify-content-center align-items-center">
+                <div className="container">
+                    <div className="row d-flex justify-content-center align-items-center">
+                        <div className="col-2 d-flex justify-content-center align-items-center">
+                            <h1 className="logo">HKVTV</h1>
+                        </div>
+                        <div className="col-8">
+                            <div className="search">
+                                <input type="text" className="form-control" placeholder="Search product" />
+                                <button className="btn btn-primary">
+                                    <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
+                                </button>
+                            </div>
+                        </div>
+                        <div className="col-2 d-flex justify-content-end align-items-center">
+                            <FontAwesomeIcon icon="fa-solid fa-cart-shopping" id="cart-icon"/>
+                            <div className="d-flex flex-column align-items-start" id="cart-info">
+                                <span>Your shopping cart</span>
+                                <span>(0) product</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                </div>
-            </nav>
-            </div>
-            <div className="container">
-            <div className="row">
-                <div className="col-2 d-flex justify-content-center align-items-center">
-                <h1 className="logo">HKVTV</h1>
-                </div>
-                <div className="col-8">
-                <div className="search">
-                    <input type="text" className="form-control" placeholder="Search product" />
-                    <button className="btn btn-primary">Search</button>
-                </div>
-                </div>
-                <div className="col-2 d-flex justify-content-end align-items-center">
-                <i className="fa-solid fa-cart-shopping" id="cart-icon" />
-                <div className="d-flex flex-column" id="cart-info">
-                    <span>Your shopping cart</span>
-                    <span>(0) product</span>
-                </div>
-                </div>
-            </div>
             </div>
         </header>
     );
