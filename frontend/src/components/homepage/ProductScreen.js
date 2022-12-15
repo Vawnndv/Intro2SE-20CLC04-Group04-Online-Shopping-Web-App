13792efa882e4f3dcaa48b86e737a9273fac4e6a
Vawnndv <dinhvanguyen810@gmail>
2022-12-15 16:24:36 +0700

    Tạo liên kết mô tả chi tiết SP - Hiện tên SP

--- frontend/src/components/homepage/ProductScreen.js
+++ frontend/src/components/homepage/ProductScreen.js
@@ -1,12 +1,49 @@
 import {useParams} from "react-router-dom";
+import {useEffect, useReducer} from "react";
+import logger from "use-reducer-logger";
+import axios from "axios";
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'FETCH_REQUEST':
+            return {...state, loading: true};
+        case 'FETCH_SUCCESS':
+            return {...state, product: action.payload, loading: false};
+        case 'FETCH_FAIL':
+            return {...state, loading:false, error: action.payload};
+        default:
+            return state;
+    }
+};
 
 function ProductScreen (){
     const params = useParams();
     const  {slug} = params;
-    return (
-        <div>
-            <h1>{slug}</h1>
-        </div>
-    )
+
+    const [{loading, error, product}, dispatch] = useReducer(reducer, {
+        loading: true,
+        error: '',
+        product: [],
+    });
+    useEffect(() => {
+        const fetchData = async () => {
+            dispatch({ type: 'FETCH_REQUEST'});
+            try {
+                const result = await axios.get(`/api/products/slug/${slug}`);
+                dispatch({type: 'FETCH_SUCCESS', payload: result.data});
+            } catch (err) {
+                dispatch({type: 'FETCH_FAIL', payload: err.message});
+            };
+        };
+        fetchData();
+    }, [slug]);
+
+    return loading? (
+        <div>Loading...</div>
+    ): error? (
+        <div>{error}</div>
+    ) : (
+        <div>{product.name}</div>
+    );
 }
 export default ProductScreen;
