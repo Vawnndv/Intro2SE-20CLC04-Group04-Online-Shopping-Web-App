6b4739ae7c155b2b09d5e5f84ee40837330f25b9
TruongGiaTien <truongtien577@gmail.com>
2022-12-16 16:19:13 +0700

    hiệu ứng hover sản phẩm chỉnh lại container trang homepage

--- frontend/src/components/product/product.js
+++ frontend/src/components/product/product.js
@@ -6,29 +6,33 @@ function Product(props) {
     const {product} = props;
     return (
         <div className="product" >
-            <Link to={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name}/>
-            </Link>
+            <div className="hover-text">Xem chi tiết</div>
+            <div className="details">
+                <Link to={`/product/${product.slug}`} className="product-thumbnail" style={{ textDecoration: 'none' }}>
+                    <img src={product.image} alt={product.name}/>
+                    {/* <div className="buffer">Xem chi tiết</div> */}
+                </Link>
 
-            <Link to={`/product/${product.slug}`} style={{ textDecoration: 'none' }}>
-                <div className="product-info">
-                    <p className="product-name">{product.name}</p>
-                    
-                    <div className="product-info-bottom">
-                        <div className="product-info-bottom-left">
-                            <p style={{fontSize: '20px'}}>Còn {product.quanity} sản phẩm</p>
-                            <p className="product-info-price">{formatPrice(product.price)}</p>
-                            <Rating rating={product.rating} numReviews={product.reviews}/>
+                <Link to={`/product/${product.slug}`} style={{ textDecoration: 'none' }}>
+                    <div className="product-info">
+                        <p className="product-name">{product.name}</p>
+                        
+                        <div className="product-info-bottom">
+                            <div className="product-info-bottom-left">
+                                <p style={{fontSize: '20px'}}>Còn {product.quanity} sản phẩm</p>
+                                <p className="product-info-price">{formatPrice(product.price)}đ</p>
+                                <Rating rating={product.rating} numReviews={product.reviews}/>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </Link>
+                </Link>
+            </div>
         </div>
     )
 };
 
 function formatPrice(price){
-    price = price.toLocaleString('it-IT', {style : 'currency', currency : 'VND'});
+    price = price.toLocaleString('it-IT');
     return price;
 }
 
