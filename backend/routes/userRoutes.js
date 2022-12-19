20afc9da14f17349049d52b40e00a37a9ffa74b1
ShinCoder <88935125+ShinCoder@users.noreply.github.com>
2022-12-19 21:22:45 +0700

    fix conflict

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -9,9 +9,9 @@ const userRouter = express.Router();
 userRouter.post(
     '/login',
     expressAsyncHandler(async (req, res) => {
-        const user = await User.findOne({ }) //email: req.body.email });
+        const user = await User.findOne({ email: req.body.email });
         if (user) {
-            // if (bcrypt.compareSync(req.body.password, user.password)) {
+            if (bcrypt.compareSync(req.body.password, user.password)) {
                 res.send({
                     _id: user._id,
                     name: user.name,
@@ -23,7 +23,7 @@ userRouter.post(
                     token: generateToken(user)
                 });
             }
-        // }
+        }
         else res.status(401).send({message: 'Email hoặc mật khẩu không đúng'});
     })
 );
